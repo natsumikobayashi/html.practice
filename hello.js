e4e5acc44241251c4b576941761bd136f0992983
natsumikobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2023-05-18 13:52:13 +0000

    [Finish]javascript2.2.2

--- hello.js
+++ hello.js
@@ -1,8 +1,26 @@
 console.log("hello, world!", "how's it going?");
 
-var text = "おいしい";
-var text2 = text + "店";
-var text3 = `${text}店`;
+const text = "おいしい";
+const text2 = text + "店";
+const text3 = `${text}店`;
 
 console.log(text2);
 console.log(text3);
+
+let text4 = "新しい";
+text4 ="古い";
+const text5 = text4 + "店";
+const text6 = `${text4}店`;
+
+console.log(text5);
+console.log(text6);
+
+const prefecture = "東京都";
+const city = "板橋区";
+const number ="0-0000";
+
+let home = `${prefecture}${city}${number}`;
+console.log(home);
+
+home = `${prefecture} ${city} ${number}`;
+console.log(home);
